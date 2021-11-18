819cb68cfc704712b62382fc5d1c091236e53027
VladanS10 <vladan.nish@gmail.com>
2021-11-18 20:56:04 +0100

    initial domaci

---
+++ D_1_6520_Vladan_Stojic/app.js
@@ -0,0 +1,18 @@
+const texts = ['Frontend Developer', 'Football Player', 'Gamer'];
+let count = 0;
+let index = 0;
+let currentText = '';
+
+(function type(){
+    if(count === texts.length){
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 300);
+}());
